1dfd7c6ed4913bd80faeea2ecf8e71146c01cdf5
unknown <punampatil2282gmail.com>
2023-05-09 15:55:31 +0530

    docker added

---
+++ src/main/java/com/capstone/employee/exception/EmployeeIdException.java
@@ -0,0 +1,9 @@
+package com.capstone.employee.exception;
+
+public class EmployeeIdException extends Exception {
+	public EmployeeIdException(String message) {
+		super(message);
+	}
+
+
+}
---
+++ src/test/java/com/capstone/employee/EmployeeSystemApplicationTests.java
@@ -0,0 +1,12 @@
+package com.capstone.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSystemApplicationTests {
+
+	/*
+	 * @Test void contextLoads() { }
+	 */
+}
